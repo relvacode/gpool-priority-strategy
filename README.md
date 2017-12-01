2713d8261ebc319df847523943b823e8d2edff19
Jason Kingsbury <jkingsbury@ukch-ofclt2695.chuk.illumina.com>
2017-12-01 15:30:33 +0000

    more docs, better README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # gpool-priority-strategy
-Priority Based Scheduling for gpool
+Priority based scheduling strategy for gpool
+
+For use with the Go concurrent execution toolkit [gpool](https://github.com/relvacode/gpool)
+
+## Usage
+
+```go
+sg := strategy.Strategy{
+    AgeFactor: 1000,
+    PriorityFactor: 2000,
+}
+br := gpool.NewSimpleBridge(5, sg)
+p := gpool.New(true, br)
+```
--- strategy.go
+++ strategy.go
@@ -39,7 +39,12 @@ func max(a, b float64) float64 {
 
 // Strategy is an evaluater for gpool that executes the highest priority job
 type Strategy struct {
-	AgeFactor      float64
+	// AgeFactor is the importance of the jobs age, calculated
+	// from the difference between the QueuedOn date and the current time.
+	// A higher AgeFactor makes the oldest jobs more important.
+	AgeFactor float64
+	// PriorityFactor is the importants of a jobs priority.
+	// A higher PriorityFactor makes the highest priority jobs more important.
 	PriorityFactor float64
 }
 
@@ -56,7 +61,7 @@ func (sg *Strategy) Evaluate(jobs []*gpool.JobStatus) (int, bool) {
 		return 0, false
 	}
 
-	// Find the oldest job
+	// Find highest values for priority and age
 	var maxAge float64
 	var maxPri float64
 	for i := 0; i < len(jobs); i++ {
@@ -72,15 +77,13 @@ func (sg *Strategy) Evaluate(jobs []*gpool.JobStatus) (int, bool) {
 	maxAge = max(maxAge, 1)
 	maxPri = max(maxPri, 1)
 
-	priorities := make([]float64, len(jobs))
-	for i := 0; i < len(jobs); i++ {
-		priorities[i] = sg.Priority(jobs[i], maxAge, maxPri)
-	}
+	// Pick the highest calculated priority
 	var pmax float64
 	var ppos int
-	for i := 0; i < len(priorities); i++ {
-		if priorities[i] > pmax {
-			pmax = priorities[i]
+	for i := 0; i < len(jobs); i++ {
+		priority := sg.Priority(jobs[i], maxAge, maxPri)
+		if priority > pmax {
+			pmax = priority
 			ppos = i
 		}
 	}
